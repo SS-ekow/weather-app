4ac22fc712d06ddcc192ae9554b124553c5f5668
SS-ekow <120583131+SS-ekow@users.noreply.github.com>
2024-03-26 16:48:08 +0000

    api requests completed

--- next-weather-app/src/app/page.tsx
+++ next-weather-app/src/app/page.tsx
@@ -1,7 +1,82 @@
+'use client'
+
 import Navbar from "@/components/navbar";
 import Image from "next/image";
+import { useQuery } from "react-query";
+import axios from "axios";
+interface WeatherData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: WeatherInfo[];
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
+}
+
+interface WeatherInfo {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+}
+
+
 
 export default function Home() {
+
+  const {isLoading, error, data}= useQuery<WeatherData>(
+    'repoData',
+     async ()=>
+  {
+    const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=accra&appid=${process.env.NEXT_WEATHER_APP_KEY}&cnt=5`
+    );
+    return data;
+  }   
+  );
+
+   console.log('data', data);
+   if (isLoading) return "Loading...";
+
+
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
