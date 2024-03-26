20c3c1bc4ec47f9f408e51df90d1cd8b14ccd7e7
SS-ekow <120583131+SS-ekow@users.noreply.github.com>
2024-03-26 23:25:14 +0000

    updated

--- next-weather-app/src/app/page.tsx
+++ next-weather-app/src/app/page.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/navbar";
 import Image from "next/image";
 import { useQuery } from "react-query";
 import axios from "axios";
+import { format, parseISO } from "date-fns";
 interface WeatherData {
   cod: string;
   message: number;
@@ -67,19 +68,41 @@ export default function Home() {
     'repoData',
      async ()=>
   {
-    const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=accra&appid=${process.env.NEXT_WEATHER_APP_KEY}&cnt=5`
+    const {data} = await axios.get('https://api.openweathermap.org/data/2.5/forecast?q=accra&appid=fcc2bc7ae0293ef2b46ff62bc9755a22&cnt=5'
     );
     return data;
   }   
   );
 
+  const firstData= data?.list[0]
+
    console.log('data', data);
-   if (isLoading) return "Loading...";
+   if (isLoading) return (
+    <div className="flex items-center min-h-screen justify-center">
+      <p className="animate-bounce">Loading...</p>
+    </div>
+   );
 
 
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
-      <Navbar />
+      <Navbar/>
+      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
+        {/* today data section */}
+        <section className="today">
+          <div>
+            <h2 className="flex gap-1 text-2xl items-end">
+               <p className="text-lg">{format(parseISO(firstData?.dt_txt ?? ""), "EEEE")}</p>
+               <p className="text-lg">({format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")})</p>
+            </h2>
+          </div>
+
+        </section>
+
+        {/* seven day forecast data */}
+        <section className="forecast"></section>
+      </main>
+      
     </div>
   );
 }
